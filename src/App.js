84c388bb160cde9f0f0f4a5ca89a73158b8dcf4f
OtakuBoy420 <U28282@rts-365.dk>
2022-09-12 12:07:11 +0200

    tailwind init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-	return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="primary-gradient w-fit text-9xl font-bold">TEST</h1>
+    </div>
+  );
 }
 
 export default App;
