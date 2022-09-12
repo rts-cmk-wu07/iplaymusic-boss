5fe91bb20811eca6964eea078cde6a6aec31d253
Sebastian Aage Nielsen <u33674@rts-365.dk>
2022-09-12 09:26:42 +0200

    react init

---
+++ src/Layout.js
@@ -0,0 +1,4 @@
+// Overall layout of the application
+// This is the main component that is rendered by the application
+// It contains the header, the main content, and the footer
+// Here goes the Outlet
