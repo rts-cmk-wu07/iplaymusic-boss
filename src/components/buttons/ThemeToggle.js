b8c34b52b7277cbe768991f3ebf9153b20ea54c1
OtakuBoy420 <U28282@rts-365.dk>
2022-09-13 14:14:47 +0200

    work in progress

--- src/components/buttons/ThemeToggle.js
+++ src/components/buttons/ThemeToggle.js
@@ -1,102 +1,79 @@
-import FeatherIcon from 'feather-icons-react';
-import { container, sun, moon } from '../../assets/variants/ThemeToggle';
-import { motion } from 'framer-motion';
-import { useState } from 'react';
-
+import FeatherIcon from "feather-icons-react";
+import { container, sun, moon } from "../../assets/variants/ThemeToggle";
+import { motion } from "framer-motion";
+import { useState } from "react";
 const ThemeToggle = () => {
-	// State for changing variants later in code
-	const [themeToggle, setThemeToggle] = useState(
-		localStorage.getItem('theme') === 'dark'
-			? 'dark'
-			: localStorage.getItem('theme') === 'light'
-			? 'light'
-			: 'auto'
-	);
+  // State for changing variants later in code
+  const [themeToggle, setThemeToggle] = useState(
+    localStorage.getItem("theme") === "dark" ? "dark" : localStorage.getItem("theme") === "light" ? "light" : "auto"
+  );
 
-	// Initial theme setup on load
-	if (
-		localStorage.theme === 'dark' ||
-		(!('theme' in localStorage) &&
-			window.matchMedia('(prefers-color-scheme: dark)').matches)
-	) {
-		document.documentElement.classList.add('dark');
-	} else {
-		document.documentElement.classList.remove('dark');
-	}
+  // Initial theme setup on load
+  if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
 
-	// Checks if the user has a theme preference set
-	// If not, it checks the system preference and sets theme
-	const darkTheme = window.matchMedia('(prefers-color-scheme: dark)');
+  // Checks if the user has a theme preference set
+  // If not, it checks the system preference and sets theme
+  const darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-	darkTheme.addEventListener('change', e => {
-		if (e.matches) {
-			themeToggle === 'auto' && document.documentElement.classList.add('dark');
-		} else {
-			themeToggle === 'auto' &&
-				document.documentElement.classList.remove('dark');
-		}
-	});
+  darkTheme.addEventListener("change", (e) => {
+    if (e.matches) {
+      themeToggle === "auto" && document.documentElement.classList.add("dark");
+    } else {
+      themeToggle === "auto" && document.documentElement.classList.remove("dark");
+    }
+  });
 
-	// Toggles theme
-	const toggleTheme = () => {
-		if (localStorage.getItem('theme') === 'light') {
-			localStorage.theme = 'dark';
-			setThemeToggle('dark');
-		} else if (localStorage.getItem('theme') === 'dark') {
-			localStorage.removeItem('theme');
-			setThemeToggle('auto');
-		} else {
-			localStorage.theme = 'light';
-			setThemeToggle('light');
-		}
-	};
+  // Toggles theme
+  const toggleTheme = () => {
+    if (localStorage.getItem("theme") === "light") {
+      localStorage.theme = "dark";
+      setThemeToggle("dark");
+    } else if (localStorage.getItem("theme") === "dark") {
+      localStorage.removeItem("theme");
+      setThemeToggle("auto");
+    } else {
+      localStorage.theme = "light";
+      setThemeToggle("light");
+    }
+  };
 
-	// Corrects framer motion transform to rotate first
-	const rayTemplate = ({ rotate, x }) => {
-		return `rotate(${rotate}) translateX(${x})`;
-	};
+  // Corrects framer motion transform to rotate first
+  const rayTemplate = ({ rotate, x }) => {
+    return `rotate(${rotate}) translateX(${x})`;
+  };
 
-	// Array for mapping "rays" in the sun
-	const raysArray = [0, 1, 2, 3, 4, 5, 6, 7];
+  // Array for mapping "rays" in the sun
+  const raysArray = [0, 1, 2, 3, 4, 5, 6, 7];
 
-	return (
-		<motion.button
-			variants={container}
-			className="h-8 w-8 flex items-center justify-center rounded-full"
-			animate={
-				themeToggle === 'light'
-					? 'light'
-					: themeToggle === 'dark'
-					? 'dark'
-					: 'auto'
-			}
-			whileTap={{ scale: 0.9 }}
-			onClick={toggleTheme}
-		>
-			<motion.div className="absolute w-6 h-6 flex items-center justify-center">
-				<motion.div
-					className="absolute w-4 h-4 border-4 rounded-full border-primary"
-					variants={sun.circle}
-				></motion.div>
-				{raysArray.map(i => (
-					<motion.span
-						transformTemplate={rayTemplate}
-						key={i}
-						custom={i}
-						className="absolute h-1 rounded-lg bg-primary"
-						variants={sun.rays}
-					></motion.span>
-				))}
-			</motion.div>
-			<motion.div className="absolute w-6 h-6 text-primary" variants={moon}>
-				<FeatherIcon
-					icon="moon"
-					strokeWidth={themeToggle === 'auto' ? '4' : '4'}
-					className="w-full h-full"
-				/>
-			</motion.div>
-		</motion.button>
-	);
+  return (
+    <motion.button
+      variants={container}
+      className="h-8 w-8 flex items-center justify-center rounded-full"
+      animate={themeToggle === "light" ? "light" : themeToggle === "dark" ? "dark" : "auto"}
+      whileTap={{ scale: 0.9 }}
+      onClick={toggleTheme}
+    >
+      <motion.div className="absolute w-6 h-6 flex items-center justify-center">
+        <motion.div className="absolute w-4 h-4 border-4 rounded-full border-primary" variants={sun.circle}></motion.div>
+        {raysArray.map((i) => (
+          <motion.span
+            transformTemplate={rayTemplate}
+            key={i}
+            custom={i}
+            className="absolute h-1 rounded-lg bg-primary"
+            variants={sun.rays}
+          ></motion.span>
+        ))}
+      </motion.div>
+      <motion.div className="absolute w-6 h-6 text-primary" variants={moon}>
+        <FeatherIcon icon="moon" strokeWidth={themeToggle === "auto" ? "4" : "4"} className="w-full h-full" />
+      </motion.div>
+    </motion.button>
+  );
 };
 
 export default ThemeToggle;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,104 +1,111 @@
-import ColoredListItem from '../components/lists/ColoredListItem';
-import FeaturedItem from '../components/lists/FeaturedItem';
-import List from '../components/lists/List';
+import { useState } from "react";
+import ColoredListItem from "../components/lists/ColoredListItem";
+import FeaturedItem from "../components/lists/FeaturedItem";
+import List from "../components/lists/List";
+import Welcome from "../components/modals/Welcome";
 
 const Home = () => {
-	const getTimeOfDay = () => {
-		const date = new Date();
-		const hours = date.getHours();
-		if (hours < 12) {
-			return 'Good morning';
-		} else if (hours >= 12 && hours <= 17) {
-			return 'Good afternoon';
-		} else {
-			return 'Good evening';
-		}
-	};
+  const getTimeOfDay = () => {
+    const date = new Date();
+    const hours = date.getHours();
+    if (hours < 12) {
+      return "Good morning";
+    } else if (hours >= 12 && hours <= 17) {
+      return "Good afternoon";
+    } else {
+      return "Good evening";
+    }
+  };
 
-	const greeting = getTimeOfDay();
+  const greeting = getTimeOfDay();
 
-	const topLinks = [
-		{
-			id: 1,
-			name: 'All Playlists',
-			link: '/playlists',
-			colorStart: 'from-extra-600',
-			colorEnd: 'to-extra-700',
-			span: 'col-span-2',
-		},
-		{
-			id: 2,
-			name: 'All Songs',
-			link: '/songs',
-			colorStart: 'from-extra-800',
-			colorEnd: 'to-extra-700',
-		},
-		{
-			id: 3,
-			name: 'All Albums',
-			link: '/albums',
-			colorStart: 'from-extra-200',
-			colorEnd: 'to-extra-300',
-		},
-		{
-			id: 4,
-			name: 'All Artists',
-			link: '/artists',
-			colorStart: 'from-extra-400',
-			colorEnd: 'to-extra-500',
-		},
-		{
-			id: 5,
-			name: 'Categories',
-			link: '/categories',
-			colorStart: 'from-extra-100',
-			colorEnd: 'to-extra-200',
-		},
-	];
+  const topLinks = [
+    {
+      id: 1,
+      name: "All Playlists",
+      link: "/playlists",
+      colorStart: "from-extra-600",
+      colorEnd: "to-extra-700",
+      span: "col-span-2",
+    },
+    {
+      id: 2,
+      name: "All Songs",
+      link: "/songs",
+      colorStart: "from-extra-800",
+      colorEnd: "to-extra-700",
+    },
+    {
+      id: 3,
+      name: "All Albums",
+      link: "/albums",
+      colorStart: "from-extra-200",
+      colorEnd: "to-extra-300",
+    },
+    {
+      id: 4,
+      name: "All Artists",
+      link: "/artists",
+      colorStart: "from-extra-400",
+      colorEnd: "to-extra-500",
+    },
+    {
+      id: 5,
+      name: "Categories",
+      link: "/categories",
+      colorStart: "from-extra-100",
+      colorEnd: "to-extra-200",
+    },
+  ];
 
-	const tempArticles = [
-		{
-			id: 1,
-			title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-			category: 'Lorem ipsum',
-			date: '2021-09-01',
-			image: 'https://picsum.photos/200/300',
-			link: '/article/1',
-		},
-		{
-			id: 2,
-			title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-			category: 'Lorem ipsum',
-			date: '2021-09-01',
-			image: 'https://picsum.photos/200/300',
-			link: '/article/2',
-		},
-		{
-			id: 3,
-			title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-			category: 'Lorem ipsum',
-			date: '2021-09-01',
-			image: 'https://picsum.photos/200/300',
-			link: '/article/3',
-		},
-	];
+  const tempArticles = [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      category: "Lorem ipsum",
+      date: "2021-09-01",
+      image: "https://picsum.photos/200/300",
+      link: "/article/1",
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      category: "Lorem ipsum",
+      date: "2021-09-01",
+      image: "https://picsum.photos/200/300",
+      link: "/article/2",
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      category: "Lorem ipsum",
+      date: "2021-09-01",
+      image: "https://picsum.photos/200/300",
+      link: "/article/3",
+    },
+  ];
 
-	return (
-		<div className="p-6">
-			<h1 className="heading gradient-text">{greeting}</h1>
-			<div className="grid grid-cols-2 gap-2 mt-4 place-content-center">
-				{topLinks.map(link => (
-					<ColoredListItem key={link.id} {...link} />
-				))}
-			</div>
-			<h1 className="heading gradient-text mt-16 mb-4">Featured</h1>
-			<List gap="gap-12">
-				{tempArticles.map(article => (
-					<FeaturedItem key={article.id} {...article} />
-				))}
-			</List>
-		</div>
-	);
+  const [welcomeModal, setWelcomeModal] = useState(true);
+
+  return (
+    <>
+      <div className="p-6">
+        <h1 className="heading gradient-text">{greeting}</h1>
+        <div className="grid grid-cols-2 gap-2 mt-4 place-content-center">
+          {topLinks.map((link) => (
+            <ColoredListItem key={link.id} {...link} />
+          ))}
+        </div>
+        <h1 className="heading gradient-text mt-16 mb-4">Featured</h1>
+        <List gap="gap-12">
+          {tempArticles.map((article) => (
+            <FeaturedItem key={article.id} {...article} />
+          ))}
+        </List>
+      </div>
+      {welcomeModal && <Welcome />}
+    </>
+  );
 };
 
 export default Home;
