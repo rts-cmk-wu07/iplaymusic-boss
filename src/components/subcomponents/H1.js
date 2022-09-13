c9b2ef092f29b2d144adb64051e176d8d96eeda2
OtakuBoy420 <U28282@rts-365.dk>
2022-09-13 11:39:51 +0200

    H1 title component done

--- src/components/subcomponents/H1.js
+++ src/components/subcomponents/H1.js
@@ -1,5 +1,5 @@
 const H1 = ({ value }) => {
-  return <H1 classname="gradient-text heading h-8">{value}</H1>;
+  return <h1 className="gradient-text heading h-12">{value}</h1>;
 };
 
 export default H1;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,7 @@
 import H1 from "../components/subcomponents/H1";
 
 const Home = () => {
-  return (
-    <div>
-      <H1 value="Goodmorning" />
-    </div>
-  );
+  return <div>home</div>;
 };
 
 export default Home;
