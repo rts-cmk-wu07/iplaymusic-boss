9a064ee460fd1dcb351bb6ba8da23aa9846075b4
Sebastian Aage Nielsen <u33674@rts-365.dk>
2022-09-13 10:29:49 +0200

    small setup for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,25 @@
-const Home = () => {
-  return <div>home</div>
-}
+import { useState } from 'react';
 
-export default Home
+const Home = () => {
+	const getTimeOfDay = () => {
+		const date = new Date();
+		const hours = date.getHours();
+		if (hours < 12) {
+			return 'Good morning';
+		} else if (hours >= 12 && hours <= 17) {
+			return 'Good afternoon';
+		} else {
+			return 'Good evening';
+		}
+	};
+
+	const greeting = getTimeOfDay();
+
+	return (
+		<div className="p-6">
+			<h1 className="text-3xl font-bold gradient-text">{greeting}</h1>
+		</div>
+	);
+};
+
+export default Home;
