c9a4858d3ddc4b2b7f805d44696eeb597b16d571
OtakuBoy420 <U28282@rts-365.dk>
2022-09-13 17:48:50 +0200

    add gradient to navigation icons

--- src/components/lists/ColoredListItem.js
+++ src/components/lists/ColoredListItem.js
@@ -1,21 +1,20 @@
-import { NavLink } from 'react-router-dom';
-import classNames from 'classnames';
+import { NavLink } from "react-router-dom";
 
 const ColoredListItem = ({ name, colorStart, colorEnd, link, span }) => {
-	return (
-		<li
-			className={`bg-gradient-to-br ${colorStart} ${colorEnd} ${
-				span || null
-			} text-white rounded-lg font-bold list-none`}
-		>
-			<NavLink
-				className="p-8 w-full h-full flex items-center justify-center"
-				to={link}
-			>
-				{name}
-			</NavLink>
-		</li>
-	);
+  return (
+    <li
+      className={`bg-gradient-to-br ${colorStart} ${colorEnd} ${
+        span || null
+      } text-white rounded-lg font-bold list-none`}
+    >
+      <NavLink
+        className="p-8 w-full h-full flex items-center justify-center"
+        to={link}
+      >
+        {name}
+      </NavLink>
+    </li>
+  );
 };
 
 export default ColoredListItem;
