69000f9fdcc63c7e0efdaf81a02398bdf0d7c377
OtakuBoy420 <U28282@rts-365.dk>
2022-09-16 08:54:47 +0200

    categories done

--- src/components/lists/CategoryList.js
+++ src/components/lists/CategoryList.js
@@ -1,10 +1,27 @@
-import { IoEllipsisHorizontal, IoChevronForward } from "react-icons/io5";
+import { IoEllipsisHorizontal, IoChevronForward, IoChevronDown } from "react-icons/io5";
 import Collapsible from "react-collapsible";
 import { Link } from "react-router-dom";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+const styles = {
+  categoryList: css`
+    .Collapsible__trigger {
+      &.is-closed svg {
+        transform: rotate(360deg);
+        transition: 0.2s ease;
+      }
+      &.is-open svg {
+        transform: rotate(180deg);
+        transition: 0.2s ease;
+      }
+    }
+  `,
+};
 
 const CategoryList = () => {
   const categories = [
     {
+      color: "bg-categories-100",
       name: "Alternative",
       subCategories: [
         {
@@ -19,6 +36,7 @@ const CategoryList = () => {
       ],
     },
     {
+      color: "bg-categories-200",
       name: "Blues",
       subCategories: [
         {
@@ -33,6 +51,7 @@ const CategoryList = () => {
       ],
     },
     {
+      color: "bg-categories-300",
       name: "Classical",
       subCategories: [
         {
@@ -47,6 +66,7 @@ const CategoryList = () => {
       ],
     },
     {
+      color: "bg-categories-400",
       name: "Country",
       subCategories: [
         {
@@ -61,6 +81,7 @@ const CategoryList = () => {
       ],
     },
     {
+      color: "bg-categories-500",
       name: "Dance",
       subCategories: [
         {
@@ -75,6 +96,7 @@ const CategoryList = () => {
       ],
     },
     {
+      color: "bg-categories-600",
       name: "Electronic",
       subCategories: [
         {
@@ -89,6 +111,7 @@ const CategoryList = () => {
       ],
     },
     {
+      color: "bg-categories-700",
       name: "Fitness & Workout",
       subCategories: [
         {
@@ -103,6 +126,7 @@ const CategoryList = () => {
       ],
     },
     {
+      color: "bg-categories-800",
       name: "Hip-Hop/rap",
       subCategories: [
         {
@@ -117,6 +141,7 @@ const CategoryList = () => {
       ],
     },
     {
+      color: "bg-categories-900",
       name: "Industrial",
       subCategories: [
         {
@@ -132,24 +157,25 @@ const CategoryList = () => {
     },
   ];
   return (
-    <div className="mt-4 flex flex-col gap-4">
-      {categories.map((category) => (
+    <div css={styles.categoryList} className="mt-4 flex flex-col gap-4">
+      {categories.map((category, index) => (
         <Collapsible
+          contentInnerClassName="my-3 text-black dark:text-white flex justify-center flex-col gap-2"
+          transitionTime="200"
           key={category.name}
-          triggerClassName={`bg-blue-300 rounded-lg flex justify-between items-center p-4 w-full text-white text-xl font-bold`}
-          triggerOpenedClassName={`bg-blue-500 rounded-lg flex justify-between p-4 items-center w-full text-white text-xl font-bold`}
-          trigger={[category.name.toString(), <IoEllipsisHorizontal />]}
+          triggerClassName={`${category.color} rounded-lg flex justify-between items-center p-4 w-full text-white text-xl font-bold`}
+          triggerOpenedClassName={`${category.color} rounded-lg flex justify-between p-4 items-center w-full text-white text-xl font-bold`}
+          trigger={[category.name.toString(), <IoChevronDown />]}
+          contentContainerTagName="ul"
         >
-          <ul className="my-3 text-black dark:text-white flex justify-center flex-col gap-2">
-            {category.subCategories.map((subCategory, index) => (
-              <li key={index} className="flex items-center">
-                <Link to={`/category/${subCategory.name}`} className="flex items-center justify-between w-full">
-                  <p className="text-sm">{subCategory.name}</p>
-                  <IoChevronForward className="text-xl" />
-                </Link>
-              </li>
-            ))}
-          </ul>
+          {category.subCategories.map((subCategory, index) => (
+            <li key={index} className="flex items-center px-2">
+              <Link to={`/category/${subCategory.name}`} className="flex items-center justify-between w-full">
+                <p className="text-sm">{subCategory.name}</p>
+                <IoChevronForward className="text-xl" />
+              </Link>
+            </li>
+          ))}
         </Collapsible>
       ))}
     </div>
--- src/pages/AllCategories.js
+++ src/pages/AllCategories.js
@@ -3,7 +3,7 @@ import CategoryList from "../components/lists/CategoryList";
 const AllCategories = () => {
   return (
     <div className="p-6">
-      <h1 className="heading gradient-text">All Categories</h1>
+      <h1 className="heading gradient-text">Categories</h1>
       <CategoryList />
     </div>
   );
