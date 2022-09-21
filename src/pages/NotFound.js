28533aef20ae41ac7abb751751a7ac2d62db4185
Sebastian Aage Nielsen <u33674@rts-365.dk>
2022-09-21 09:23:05 +0200

    temp

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,27 @@
+import { useEffect } from 'react';
+import villager from '../assets/sounds/villager.mp3';
+import villagerImg from '../assets/images/villager.png';
+
 const NotFound = () => {
-	return <div></div>;
+	const randomWait = () => {
+		setTimeout(() => {
+			const audio = new Audio(villager);
+			audio.play();
+			randomWait();
+		}, Math.random() * 10000 + 10000);
+	};
+
+	useEffect(() => {
+		randomWait();
+	});
+
+	return (
+		<div>
+			<h1>404</h1>
+			<img src={villagerImg} alt="villager" />
+			<audio src={villager} autoPlay />
+		</div>
+	);
 };
 
 export default NotFound;
