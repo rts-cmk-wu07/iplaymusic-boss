c5e97db4069b931ef9dbbf6b01c52fe9ccb89db9
Sebastian Aage Nielsen <u33674@rts-365.dk>
2022-09-21 10:54:14 +0200

    audio player test

--- src/templates/Player.js
+++ src/templates/Player.js
@@ -1,18 +1,34 @@
 import { motion } from 'framer-motion';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import MiniPlayer from './MiniPlayer';
 import LargePlayer from './LargePlayer';
+import useFetch from '../hooks/useFetch';
+import SpotifyPlayer from 'react-spotify-web-playback';
+import TokenContext from '../contexts/TokenContext';
 
 const Player = ({ isPlaying, song }) => {
+	const { tokenData } = useContext(TokenContext);
+	const rickrollid = '4cOdK2wGLETKBW3PvgPWqT'; // rickroll
+	const { data, loading, error } = useFetch(
+		`https://api.spotify.com/v1/tracks/${rickrollid}`
+	);
+	console.log(data);
+	song = data;
+	const {
+		data: audioData,
+		loading: audioLoading,
+		error: audioError,
+	} = useFetch(`https://api.spotify.com/v1/audio-features/${rickrollid}`);
+	console.log(audioData);
 	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<motion.section
 			drag="y"
 			dragConstraints={{ top: 0, bottom: 0 }}
-			onMouseOut={() => {
+			onDragEnd={() => {
 				setIsOpen(!isOpen);
-				console.log('mouse up');
 			}}
+			dragTransition={{ bounceStiffness: 300, bounceDamping: 15 }}
 			layout
 			animate={{ borderRadius: isOpen ? '0px' : '8px' }}
 			transition={{ duration: 0.3, ease: 'easeInOut' }}
@@ -22,6 +38,7 @@ const Player = ({ isPlaying, song }) => {
 					: 'bottom-20 left-2 right-2 p-1'
 			}`}
 		>
+			<SpotifyPlayer token={tokenData.accessToken} uris={[data.uri]} />
 			<LargePlayer isOpen={isOpen} setIsOpen={setIsOpen} song={song} />
 			<MiniPlayer isOpen={isOpen} setIsOpen={setIsOpen} song={song} />
 		</motion.section>
