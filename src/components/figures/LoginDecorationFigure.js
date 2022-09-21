c6981016ad73a44838a33347fff1b415d385f4c3
OtakuBoy420 <U28282@rts-365.dk>
2022-09-21 13:08:10 +0200

    work in progress

---
+++ src/components/figures/LoginDecorationFigure.js
@@ -0,0 +1,59 @@
+import { motion } from "framer-motion";
+const LoginDecorationFigure = () => {
+  return (
+    <motion.svg
+      animate={{
+        rotate: [0, 3, -3, 0],
+        y: [0, 15, -15, 0],
+        x: [0, 50, -50, 0],
+        scale: [1.3, 1.4, 1.4, 1.3],
+        transition: {
+          duration: 5,
+          repeat: Infinity,
+        },
+      }}
+      initial={{
+        rotate: 0,
+        y: 0,
+        x: 0,
+        scale: 1.3,
+      }}
+      xmlns="http://www.w3.org/2000/svg"
+      width="590.891"
+      height="480.76"
+      viewBox="0 0 590.891 480.76"
+    >
+      <defs>
+        <linearGradient
+          id="linear-gradient"
+          y1="0.5"
+          y2="0.5"
+          x2="1"
+          gradientUnits="objectBoundingBox"
+        >
+          <stop offset="0" stopColor="#ee0979"></stop>
+          <stop offset="1" stopColor="#ff6a00"></stop>
+        </linearGradient>
+      </defs>
+      <g data-name="Asset 2" transform="rotate(-165.02 292.362 206.02)">
+        <g transform="translate(-.001 -.003)">
+          <path
+            fill="#f2bc06"
+            d="M252.459 172.079c46.331 1.937 88.885-28.55 120.787-64.964 26.226-29.945 47.648-64.5 72.654-95.49-1.356-.194-2.712-.426-4.068-.581-37.5-4.532-74.727 4.261-111.393 12.609s-74.165 16.27-111.548 10.072c-31.146-5.268-60.916-20.067-91.713-28.3S62.331-3.658 36.609 17.048C7.187 40.678-4.086 88.036 1.3 131.714c15.592-14.527 34.109-24.638 54.583-27.33 69.166-9.104 126.963 64.867 196.576 67.695z"
+            data-name="Path 638"
+            transform="translate(.001 .003)"
+          ></path>
+
+          <path
+            fill="#f18d05"
+            d="M445.272 6c-25.006 30.991-46.486 65.642-72.654 95.587-31.9 36.414-74.455 66.863-120.787 64.964-69.613-2.925-127.411-76.9-196.579-67.734C34.779 101.51 16.262 111.62.67 126.089a222.945 222.945 0 001.937 12.551 223.251 223.251 0 0018.459 53.614c25.3-9.685 55.222-9.162 81.583-1.356 42.167 12.493 79.259 39.494 119.489 58.108s88.227 28.027 126.539 5.21c35.407-21.054 53.711-63.919 77.787-99a270.124 270.124 0 01109.611-92.348C514.594 29.359 479.73 11.094 445.272 6z"
+            data-name="Path 640"
+            transform="translate(.629 5.627)"
+          ></path>
+        </g>
+      </g>
+    </motion.svg>
+  );
+};
+
+export default LoginDecorationFigure;
--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,24 +1,31 @@
-import querystring from 'querystring';
+import querystring from "querystring";
+import { motion } from "framer-motion";
+import { BsSpotify } from "react-icons/bs";
+import LoginDecorationFigure from "../components/figures/LoginDecorationFigure";
 export default function Login() {
-	// get client id from environment variable
-	const clientId = process.env.REACT_APP_CLIENT_ID;
-	var queryParameters = querystring.stringify({
-		response_type: 'code',
-		client_id: clientId,
-		scope: 'user-read-private user-read-email',
-		redirect_uri: 'http://127.0.0.1:8888/callback',
-		state: '324y732467234763284678324htr',
-	});
-	return (
-		<>
-			<h1>Login</h1>
-			<a
-				className="text-2xl bg-red-700"
-				href={`https://accounts.spotify.com/authorize?${queryParameters}`}
-			>
-				Log in with spotify
-			</a>
-			<br />
-		</>
-	);
+  // get client id from environment variable
+  const clientId = process.env.REACT_APP_CLIENT_ID;
+  var queryParameters = querystring.stringify({
+    response_type: "code",
+    client_id: clientId,
+    scope: "user-read-private user-read-email",
+    redirect_uri: "http://127.0.0.1:8888/callback",
+    state: "324y732467234763284678324htr",
+  });
+  return (
+    <div className="w-full overflow-hidden h-screen gradient dark:bg-additional flex flex-col items-center">
+      <div className="relative -top-32 -left-4 h-1/2">
+        <LoginDecorationFigure />
+      </div>
+      <div className="flex flex-col gap-4">
+        <h2 className="heading text-white">iPlayMusic</h2>
+        <a
+          className="rounded-xl border-2 border-white p-3 flex items-center justify-between text-white"
+          href={`https://accounts.spotify.com/authorize?${queryParameters}`}
+        >
+          Log in with spotify <BsSpotify color="#1ED760" size="25" />
+        </a>
+      </div>
+    </div>
+  );
 }
