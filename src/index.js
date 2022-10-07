48b072468120952aa04a8f2996f1788a8391cb31
Sebastian Aage Nielsen <u33674@rts-365.dk>
2022-10-07 12:34:02 +0200

    maybe pwa

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import PlayerLayout from "./templates/PlayerLayout";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <PlayerLayout>
-      <App />
-    </PlayerLayout>
-  </Router>
+	<Router>
+		<PlayerLayout>
+			<App />
+		</PlayerLayout>
+	</Router>
 );
+
+serviceWorkerRegistration.register();
