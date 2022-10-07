28d8244598840ae6999091725903cf89ff70c3a1
Sebastian Aage Nielsen <u33674@rts-365.dk>
2022-10-07 12:52:33 +0200

    small fixes and tweaks

--- src/components/subcomponents/SongListItem.js
+++ src/components/subcomponents/SongListItem.js
@@ -5,94 +5,101 @@ import useSong from "../../hooks/useSong";
 import { FiChevronRight } from "react-icons/fi";
 import SongContext from "../../contexts/SongContext";
 import NowPlaying from "./NowPlaying";
+import { AnimatePresence } from "framer-motion";
 
 const SongListItem = ({
-  track,
-  index,
-  noImage,
-  largePadding,
-  searchCard,
-  albumId,
+	track,
+	index,
+	noImage,
+	largePadding,
+	searchCard,
+	albumId,
 }) => {
-  // State for the play/pause button
-  const updateSong = useSong(track?.id);
-  const [isImage, setIsImage] = useState(false);
-  const { songData } = useContext(SongContext);
-  let isExists =
-    track?.album?.images.length > 0 ? true : noImage ? true : false;
-  const navigate = useNavigate();
-  return (
-    isExists && (
-      <li
-        key={index || track?.id}
-        onClick={updateSong}
-        className={`flex items-center gap-2 justify-between w-full ${
-          largePadding ? "py-2" : "py-1"
-        } text-additional
-      dark:text-white select-none`}>
-        <div className="flex justify-between items-center flex-shrink-0">
-          {index && (
-            <p className="w-6 font-light tracking-widest text-sm text-left shrink-0">
-              {index}
-            </p>
-          )}
-          {!noImage && (
-            <div className="w-12 h-12 rounded-md relative overflow-hidden">
-              {!isImage && (
-                <div className="absolute bg-slate-500 z-10 w-full aspect-square"></div>
-              )}
-              <img
-                src={track?.album?.images[0]?.url}
-                alt="album cover"
-                onLoad={() => setIsImage(true)}
-                className="absolute aspect-square"
-              />
-            </div>
-          )}
-        </div>
-        <div className="flex flex-col min-w-0 whitespace-nowrap">
-          <div onClick={updateSong} className="flex items-baseline gap-1">
-            {songData?.id === track?.id && <NowPlaying />}
-            <p
-              className={
-                songData?.id === track?.id
-                  ? "text-primary text-md font-bold text-ellipsis overflow-hidden"
-                  : "text-md font-bold text-ellipsis overflow-hidden"
-              }>
-              {track?.name}
-            </p>
-          </div>
-          <div className="flex min-w-0 text-ellipsis overflow-hidden whitespace-nowrap">
-            {track?.artists?.map((artist, index) => {
-              return (
-                <Link
-                  to={`/artist/${artist?.id}`}
-                  key={index}
-                  className="text-xs opacity-75"
-                  onClick={(e) => e.stopPropagation()}>
-                  {(index ? ", " : "") + artist?.name}
-                </Link>
-              );
-            })}
-          </div>
-        </div>
-        {searchCard ? (
-          <p
-            className="ml-auto text-2xl"
-            onClick={(event) => {
-              event.stopPropagation();
-              navigate(`/album/${albumId}`);
-            }}>
-            <FiChevronRight />
-          </p>
-        ) : (
-          <p className="text-black/75 dark:text-white/75 ml-auto text-sm flex-shrink-0">
-            {millisToTime(track?.duration_ms)}
-          </p>
-        )}
-      </li>
-    )
-  );
+	// State for the play/pause button
+	const updateSong = useSong(track?.id);
+	const [isImage, setIsImage] = useState(false);
+	const { songData } = useContext(SongContext);
+	let isExists =
+		track?.album?.images.length > 0 ? true : noImage ? true : false;
+	const navigate = useNavigate();
+	return (
+		isExists && (
+			<li
+				key={index || track?.id}
+				onClick={updateSong}
+				className={`flex items-center gap-2 justify-between w-full ${
+					largePadding ? "py-2" : "py-1"
+				} text-additional
+      dark:text-white select-none`}
+			>
+				<div className="flex justify-between items-center flex-shrink-0">
+					{index && (
+						<p className="w-6 font-light tracking-widest text-sm text-left shrink-0">
+							{index}
+						</p>
+					)}
+					{!noImage && (
+						<div className="w-12 h-12 rounded-md relative overflow-hidden">
+							{!isImage && (
+								<div className="absolute bg-slate-500 z-10 w-full aspect-square"></div>
+							)}
+							<img
+								src={track?.album?.images[0]?.url}
+								alt="album cover"
+								onLoad={() => setIsImage(true)}
+								className="absolute aspect-square"
+							/>
+						</div>
+					)}
+				</div>
+				<div className="flex flex-col min-w-0 whitespace-nowrap">
+					<div onClick={updateSong} className="flex items-baseline">
+						<AnimatePresence>
+							{songData?.id === track?.id && <NowPlaying />}
+						</AnimatePresence>
+						<p
+							className={
+								songData?.id === track?.id
+									? "text-primary text-md font-bold text-ellipsis overflow-hidden"
+									: "text-md font-bold text-ellipsis overflow-hidden"
+							}
+						>
+							{track?.name}
+						</p>
+					</div>
+					<div className="flex min-w-0 text-ellipsis overflow-hidden whitespace-nowrap">
+						{track?.artists?.map((artist, index) => {
+							return (
+								<Link
+									to={`/artist/${artist?.id}`}
+									key={index}
+									className="text-xs opacity-75"
+									onClick={e => e.stopPropagation()}
+								>
+									{(index ? ", " : "") + artist?.name}
+								</Link>
+							);
+						})}
+					</div>
+				</div>
+				{searchCard ? (
+					<p
+						className="ml-auto text-2xl"
+						onClick={event => {
+							event.stopPropagation();
+							navigate(`/album/${albumId}`);
+						}}
+					>
+						<FiChevronRight />
+					</p>
+				) : (
+					<p className="text-black/75 dark:text-white/75 ml-auto text-sm flex-shrink-0">
+						{millisToTime(track?.duration_ms)}
+					</p>
+				)}
+			</li>
+		)
+	);
 };
 
 export default SongListItem;
