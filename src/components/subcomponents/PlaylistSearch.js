69cfcac9c86b0fd9dbf1e0e7b471b84b32a79590
OtakuBoy420 <U28282@rts-365.dk>
2022-10-11 14:11:54 +0200

    init

---
+++ src/components/subcomponents/PlaylistSearch.js
@@ -0,0 +1,11 @@
+const PlaylistSearch = () => {
+  return (
+    <input
+      type="text"
+      class="shadow-sm bg-primary dark:bg-additional dark:text-white px-4 py-2 rounded-lg mt-4 outline-none border-none"
+      placeholder="Search for song or artist..."
+    />
+  );
+};
+
+export default PlaylistSearch;
