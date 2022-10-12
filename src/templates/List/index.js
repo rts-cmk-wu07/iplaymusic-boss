186e24e3bcba7191af209d14c9a08bd5e295fa2d
OtakuBoy420 <U28282@rts-365.dk>
2022-10-12 14:21:24 +0200

    maybe done

--- src/templates/List/index.js
+++ src/templates/List/index.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import SongListItem from "../../components/subcomponents/SongListItem";
+import { InView } from "react-intersection-observer";
 import useFetch from "../../hooks/useFetch";
 import { useSearchParams } from "react-router-dom";
 import Loader from "../../components/subcomponents/Loader";
@@ -10,11 +11,15 @@ import trailingActions from "./TrailingActions";
 import leadingActions from "./LeadingActions";
 import useControls from "../../hooks/useControls";
 import PlaylistSearch from "../../components/subcomponents/PlaylistSearch";
+
 const List = (props) => {
-  const { startUrl, trackLocation, header, showTitle } = props;
+  const { startUrl, loadMoreOnIndex, trackLocation, header, showTitle } = props;
   // States
-  const [currentUrl] = useState(startUrl);
+  const [currentUrl, setCurrentUrl] = useState(startUrl);
   const [songArray, setSongArray] = useState(null);
+  const [nextUrl, setNextUrl] = useState(null);
+  const [inView, setInView] = useState(false);
+  const [loadMoreIndex, setLoadMoreIndex] = useState(loadMoreOnIndex);
 
   //Placheholder array of objects for the songs list items to be mapped over in the return statement below
   const { data, loading } = useFetch(currentUrl);
@@ -24,17 +29,7 @@ const List = (props) => {
   // Getting the title
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
-  /* eslint-disable */
-  useEffect(() => {
-    if (data.items) {
-      if (songArray) {
-        setSongArray([...songArray, ...data.items]);
-      } else {
-        setSongArray(data.items.filter((item) => item));
-      }
-    }
-  }, [data]);
-  /* eslint-enable */
+
   const [search, setSearch] = useState("");
   const filteredTracks = songArray?.filter((track) => {
     const trackData = trackLocation ? track[trackLocation] : track;
@@ -46,20 +41,36 @@ const List = (props) => {
       trackArtists?.join(" ").toLowerCase().includes(search.toLowerCase())
     );
   });
-
+  /* eslint-disable */
+  useEffect(() => {
+    if (data.items) {
+      if (songArray) {
+        setLoadMoreIndex((prevState) => prevState + loadMoreOnIndex + 10);
+        setSongArray([...songArray, ...data.items]);
+      } else {
+        setSongArray(data.items.filter((item) => item));
+      }
+      setNextUrl(data.next);
+    }
+  }, [data]);
+  /* eslint-enable */
+  console.log(songArray?.length);
+  // When bottom element (loadMoreOnIndex) gets show, fetch nextUrl
+  useEffect(() => {
+    if (inView) setCurrentUrl(nextUrl);
+  }, [inView, nextUrl]);
   return (
     <>
       {showTitle && (
         <h1 className="heading gradient-text">{header ? header : title}</h1>
       )}
-
+      {loading && <Loader />}
       {songArray?.length <= 0 && !loading && (
         <h2 className="heading text-addition dark:text-white text-center mt-[25%]">
           There are no songs here yet
         </h2>
       )}
       <PlaylistSearch search={search} setSearch={setSearch} />
-      {loading && <Loader />}
       {!loading && songArray && (
         <SwipeableList
           style={{
@@ -90,18 +101,38 @@ const List = (props) => {
                 textColor: "white",
               });
 
-            return (
-              <SwipeableListItem
-                key={i}
-                leadingActions={leadingAction()}
-                trailingActions={trailingAction()}>
-                <SongListItem
-                  key={trackData?.id}
-                  id={trackData?.id}
-                  track={trackData}
-                />
-              </SwipeableListItem>
-            );
+            if (i === loadMoreIndex && nextUrl) {
+              return (
+                <InView key={i} onChange={setInView}>
+                  {({ inView, ref, entry }) => (
+                    <div key={i} ref={ref}>
+                      <SwipeableListItem
+                        leadingActions={leadingAction()}
+                        trailingActions={trailingAction()}>
+                        <SongListItem
+                          key={i}
+                          id={trackData?.id}
+                          track={trackData}
+                        />
+                      </SwipeableListItem>
+                    </div>
+                  )}
+                </InView>
+              );
+            } else {
+              return (
+                <SwipeableListItem
+                  key={i}
+                  leadingActions={leadingAction()}
+                  trailingActions={trailingAction()}>
+                  <SongListItem
+                    key={trackData?.id}
+                    id={trackData?.id}
+                    track={trackData}
+                  />
+                </SwipeableListItem>
+              );
+            }
           })}
         </SwipeableList>
       )}
